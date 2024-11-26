2ef74588fa267704581f41810097ccd4bed799ad
Catevika <dominique.bello@outlook.fr>
2024-11-26 17:25:41 +0100

    Add Drizzle

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,11 +1,16 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+config({
+  path: '.env',
+});
 
 export default defineConfig({
   out: './drizzle',
-  schema: ['./src/db/productSchema.ts'],
+  schema: ['./src/db/productSchema.ts',],
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!
   },
   verbose: true,
   strict: true,
